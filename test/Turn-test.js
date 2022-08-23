6f139d7a1e8be7beaff238d10cc3d25441196f80
MatthewPress <press.matt14@gmail.com>
2022-08-22 21:22:18 -0600

    chore: add turn files and mocha test framework

---
+++ test/Turn-test.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+
+  });
+
+  it('should store a users guess', function() {
+
+  });
+
+  it('should store a card', function() {
+
+  });
+
+  it('should return users guess', function() {
+
+  });
+
+  it('should return the card', function() {
+
+  });
+
+  it('should determine if the users guess it correct', function() {
+
+  });
+
+  it('should give feedback', function() {
+
+  });
+});
