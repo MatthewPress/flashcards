c4aab377c6e4f68fc681220a9dc98c716bdf23f4
MatthewPress <press.matt14@gmail.com>
2022-08-22 21:26:57 -0600

    chore: add skips to turn test file

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,31 +5,33 @@ const Turn = require('../src/Turn');
 
 describe('Turn', function() {
 
-  it('should be a function', function() {
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+
+    expect(turn).to.be.a('function');
+  });
+
+  it.skip('should store a users guess', function() {
 
   });
 
-  it('should store a users guess', function() {
+  it.skip('should store a card', function() {
 
   });
 
-  it('should store a card', function() {
+  it.skip('should return users guess', function() {
 
   });
 
-  it('should return users guess', function() {
+  it.skip('should return the card', function() {
 
   });
 
-  it('should return the card', function() {
+  it.skip('should determine if the users guess it correct', function() {
 
   });
 
-  it('should determine if the users guess it correct', function() {
-
-  });
-
-  it('should give feedback', function() {
+  it.skip('should give feedback', function() {
 
   });
 });
