9da9046f97e3b9048f04b535b8d4e823575d6857
MatthewPress <press.matt14@gmail.com>
2022-08-23 13:44:49 -0600

    test: Turn tst file

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,36 +2,58 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
+  let turn;
+  let card;
 
-  it.skip('should be a function', function() {
-    const turn = new Turn();
-
-    expect(turn).to.be.a('function');
+  beforeEach(function() {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('object', card);
   });
 
-  it.skip('should store a users guess', function() {
-
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
   });
 
-  it.skip('should store a card', function() {
+  it('should be an instance of Turn', function() {
+    expect(turn).to.be.an.instanceof(Turn);
+  }); 
 
+  it('should store a users guess', function() {
+    expect(turn.userGuess).to.equal('object');
   });
 
-  it.skip('should return users guess', function() {
-
+  it('should store a card', function() {
+    expect(turn.card).to.deep.equal(card);
   });
 
-  it.skip('should return the card', function() {
-
+  it('should return users guess', function() {
+    expect(turn.returnGuess()).to.equal('object');
   });
 
-  it.skip('should determine if the users guess it correct', function() {
-
+  it('should return the card', function() {
+    expect(turn.returnCard()).to.deep.equal(card);
   });
 
-  it.skip('should give feedback', function() {
+  it('should know if the users guess is correct', function() {
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
 
+  it('should give feedback when answer is correct', function() {
+    expect(turn.giveFeedback()).to.equal('Correct!');
+  });
+
+  it('should know if the users guess is incorrect', function() {
+    turn = new Turn('array', card);
+
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
+  it('should give feedback when answer is incorrect', function() {
+    turn = new Turn('array', card);
+
+    expect(turn.giveFeedback()).to.equal('Incorrect!');
   });
 });
