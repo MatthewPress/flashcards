dc6fc608c656750b27a0329ed5fa9dd0781f37fa
MatthewPress <press.matt14@gmail.com>
2022-08-23 13:45:28 -0600

    feat: Turn implementation

---
+++ src/Turn.js
@@ -0,0 +1,29 @@
+class Turn {
+  constructor(userGuess, card) {
+    this.userGuess = userGuess;
+    this.card = card;
+  }
+
+  returnGuess() {
+    return this.userGuess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    if (this.userGuess === this.card.correctAnswer) {
+      return true;
+    }
+    return false;
+  }
+
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+      return `Correct!`;
+    } 
+    return `Incorrect!`;
+  }
+}
+module.exports = Turn;
