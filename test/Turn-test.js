6a1147bbf7068dde80f1d65b7cc89b58efbe3259
MatthewPress <press.matt14@gmail.com>
2022-08-23 22:05:38 -0600

    test: use a different assertion to test the deck

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
 describe('Turn', function() {
-  let turn;
   let card;
+  let turn;
 
   beforeEach(function() {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
