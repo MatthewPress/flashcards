34247166b767020c2070313c1b66e6613cbf0d48
MatthewPress <press.matt14@gmail.com>
2022-08-23 22:07:37 -0600

    feat: deck and tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,24 +1,24 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Deck = require('../src/Deck.js');
 const Card = require('../src/Card.js');
+const Deck = require('../src/Deck.js');
 
 describe('Deck', function() {
-  let deck;
   let card1;
   let card2;
   let card3;
+  let deck;
 
   beforeEach(function() {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    deck = new Deck(card1, card2, card3);
+    deck = new Deck([card1, card2, card3]);
   });
 
-  it('should have an array of card objects instances', function() {
-    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  it('should have an array of card object instances', function() {
+    expect(deck.cards.every(card => card instanceof Card)).to.equal(true);
   });
 
   it('should be able to count the number of cards in the deck', function() {
